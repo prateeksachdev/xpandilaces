6a781f6c293ec52d2012c32012d3019576b33391
Roy Bao <roy@roybao.com>
2019-05-21 11:25:16 -0700

    Fix: review section margin

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -13,7 +13,6 @@ import '../../styles/theme.scss.liquid';
 import '../sections/header';
 
 import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
-import {cookiesEnabled} from '@shopify/theme-cart';
 
 import VueAwesomeSwiper from 'vue-awesome-swiper';
 Vue.use(VueAwesomeSwiper, /* { default global options } */);
@@ -23,7 +22,7 @@ focusHash();
 bindInPageLinks();
 
 // Apply a specific class to the html element for browser support of cookies.
-if (cookiesEnabled()) {
+if (window.navigator.cookieEnabled) {
   document.documentElement.className = document.documentElement.className.replace(
     'supports-no-cookies',
     'supports-cookies',
