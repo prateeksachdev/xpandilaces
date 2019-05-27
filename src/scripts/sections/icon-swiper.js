cbf5d41ea1ae1af9798055b090e72dfcb30bf44a
Roy Bao <roy@roybao.com>
2019-05-27 16:55:58 -0700

    Chore: code cleanup

--- src/scripts/sections/icon-swiper.js
+++ src/scripts/sections/icon-swiper.js
@@ -1,37 +1,35 @@
-(function () {
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
 
-    new Vue({
-      el: 'section.icon-swiper',
-      delimiters: ['${', '}'],
-      data: function () {
-        return {
-          swiperOption: {
-            slidesPerView: 5,
-            spaceBetween: 30,
-            pagination: {
-              el: '.swiper-pagination',
-              clickable: true
+  new Vue({
+    el: 'section.icon-swiper',
+    delimiters: ['${', '}'],
+    data: function () {
+      return {
+        swiperOption: {
+          slidesPerView: 5,
+          spaceBetween: 30,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          },
+          loop: true,
+          breakpoints: {
+            // when window width is <= 575px
+            575: {
+              centeredSlides: true,
+              slidesPerView: 2,
+              spaceBetween: 20
             },
-            loop: true,
-            breakpoints: {
-              // when window width is <= 575px
-              575: {
-                centeredSlides: true,
-                slidesPerView: 2,
-                spaceBetween: 20
-              },
-              // when window width is <= 768px
-              768: {
-                centeredSlides: true,
-                slidesPerView: 1,
-                spaceBetween: 30
-              }
+            // when window width is <= 768px
+            768: {
+              centeredSlides: true,
+              slidesPerView: 1,
+              spaceBetween: 30
             }
           }
         }
       }
-    })
-
+    }
   })
-})()
+
+})
--- src/scripts/sections/three-item-banner.js
+++ src/scripts/sections/three-item-banner.js
@@ -1,36 +1,34 @@
-(function () {
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
 
-    new Vue({
-      el: 'section.three-item-banner',
-      delimiters: ['${', '}'],
-      data: function () {
-        return {
-          swiperOption: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-            pagination: {
-              el: '.swiper-pagination',
-              clickable: true
+  new Vue({
+    el: 'section.three-item-banner',
+    delimiters: ['${', '}'],
+    data: function () {
+      return {
+        swiperOption: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          },
+          breakpoints: {
+            // when window width is <= 575px
+            575: {
+              centeredSlides: true,
+              slidesPerView: 2,
+              spaceBetween: 20
             },
-            breakpoints: {
-              // when window width is <= 575px
-              575: {
-                centeredSlides: true,
-                slidesPerView: 2,
-                spaceBetween: 20
-              },
-              // when window width is <= 768px
-              768: {
-                centeredSlides: false,
-                slidesPerView: 2,
-                spaceBetween: 30
-              }
+            // when window width is <= 768px
+            768: {
+              centeredSlides: false,
+              slidesPerView: 2,
+              spaceBetween: 30
             }
           }
         }
       }
-    })
-
+    }
   })
-})()
+
+})
