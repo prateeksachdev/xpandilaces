a99dd57b71b02764383c48c1fa9548305b69c63e
Roy Bao <roy@roybao.com>
2019-06-21 23:06:45 -0400

    Feature: page.narrow template

---
+++ src/scripts/templates/page.narrow.js
@@ -0,0 +1,8 @@
+import '../../styles/templates/page.narrow.scss'
+
+import Vue from 'vue'
+import init from "../../vue/init.js";
+
+Vue.component('tab-content', require('../../vue/components/tab-content.vue').default)
+
+init()
