17082b0e04eff4c2f4c2828fe05565df13a6396e
Roy Bao <roy@roybao.com>
2019-06-21 23:46:31 -0400

    Feature: blog related pages

---
+++ src/scripts/templates/article.js
@@ -0,0 +1,6 @@
+import '../../styles/templates/article.scss'
+
+import Vue from 'vue'
+import init from "../../vue/init.js";
+
+init()
---
+++ src/scripts/templates/blog.js
@@ -0,0 +1,6 @@
+import '../../styles/templates/blog.scss'
+
+import Vue from 'vue'
+import init from "../../vue/init.js";
+
+init()
