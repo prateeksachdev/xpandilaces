12dd2a675a0266710b499fce15b1709c32d11ca5
Teddy Maulana <maulanateddy99@gmail.com>
2019-06-24 15:18:27 +0700

    Feature: FAQ on how it works page

--- src/scripts/templates/page.with-tab.js
+++ src/scripts/templates/page.with-tab.js
@@ -1,8 +1,10 @@
 import '../../styles/sections/how-it-work-tab.scss'
+import '../../styles/sections/faq.scss'
 
 import Vue from 'vue'
 import init from "../../vue/init.js";
 
 Vue.component('tab-content', require('../../vue/components/tab-content.vue').default)
+Vue.component('faq-item', require('../../vue/components/faq-item.vue').default)
 
 init()
