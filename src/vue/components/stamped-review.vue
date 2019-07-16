b3e8bc1979641cbccc8f2fcafb7549bbf431254f
Roy Bao <roy@roybao.com>
2019-07-16 11:53:54 -0700

    Fix: loading icon and text for stamped review

--- src/vue/components/stamped-review.vue
+++ src/vue/components/stamped-review.vue
@@ -2,6 +2,11 @@
   <section class="stamped-review-wrapper">
     <slot name="title"></slot>
 
+    <div v-if="isLoadingReview" class="loading-container">
+      <p style="text-align: center;"><img src="//cdn2.shopify.com/s/files/1/1206/8416/t/110/assets/loader.gif"></p>
+      <p style="text-align: center; font-size: 20px;">Loading all reviews...</p>
+    </div>
+
     <div class="reviews lazyload" @lazybeforeunveil="load">
       <slot v-if="show" name="snippet"></slot>
     </div>
@@ -13,14 +18,23 @@
     name: 'StampedReview',
     data: function () {
       return {
-        show: false
+        show: false,
+        isLoadingReview: true
       }
     },
     methods: {
       load () {
         this.show = true
 
-        setTimeout(function () {
+        var id = setInterval(() => {
+          console.log('check')
+          if (window.jQuery('.stamped-visual-gallery-media-block').length > 0) {
+            clearInterval(id)
+            this.isLoadingReview = false
+          }
+        }, 1000)
+
+        setTimeout(() => {
           if (window.jQuery) {
             window.jQuery.ajax({
               type: 'GET',
@@ -28,7 +42,7 @@
               data: {
                 shopShopifyDomain: Shopify.shop
               },
-              success: function (data) {
+              success: (data) => {
                 var appKey = data.apiKey
                 // init widgets
                 window.StampedFn.init({apiKey: appKey, storeUrl: Shopify.shop})
