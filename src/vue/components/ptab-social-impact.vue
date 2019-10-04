9b29adf4335bee0f850e71597f83a5a6d3a3877f
Roy Bao <roy@roybao.com>
2019-10-04 13:17:28 -0700

    Feature: feedback

--- src/vue/components/ptab-social-impact.vue
+++ src/vue/components/ptab-social-impact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div v-for="detail in tab.details" :key="detail.title" class="detail">
-      <h3>{{detail.title}}</h3>
+      <h3 v-html="detail.title"></h3>
       <div class="divider"></div>
       <p v-html="detail.text"></p>
       
